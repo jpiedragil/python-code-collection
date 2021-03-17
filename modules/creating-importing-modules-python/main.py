0604d72774d942719bb588c31615aa83b76264f3
Javier Piedragil <jpiedragil@gmailcom>
2021-03-17 00:47:40 -0600

    📦 NEW: Add article reference
    - Add the reference of the article that the source code is taken from.

--- modules/creating-importing-modules-python/main.py
+++ modules/creating-importing-modules-python/main.py
@@ -1,3 +1,9 @@
+'''
+Source code from "Creating and Importing Modules in Python" article,
+written by Nicholas Samuel.
+URL: https://stackabuse.com/creating-and-importing-modules-in-python/
+'''
+
 import sys
 
 sys.path.append('../../example_library/')
