1ed0e1bc9a3478c80b5d7e75c3266ea10d663db5
Javier Piedragil <jpiedragil@gmailcom>
2021-03-17 00:29:39 -0600

    📦 NEW: Add example of execution of a module
    - Add an example of how can you execute module's code with the import
     statement.

--- modules/creating-importing-modules-python/hello.py
+++ modules/creating-importing-modules-python/hello.py
@@ -2,20 +2,4 @@ def my_function():
 
     print("Hello World")
 
-# Defining our variable
-#
-name = "Nicholas"
-
-# Defining a class
-#
-class Student:
-
-    def __init__(self, name, course):
-
-        self.course = course
-        self.name = name
-
-    def get_student_details(self):
-
-        print("Your name is " + self.name + ".")
-        print("You're studying " + self.course + ".")
+my_function()
--- modules/creating-importing-modules-python/main.py
+++ modules/creating-importing-modules-python/main.py
@@ -1,8 +1,1 @@
 import hello
-
-hello.my_function()
-
-print(hello.name)
-
-nicholas = hello.Student("Nicholas", "Computer Science")
-nicholas.get_student_details()
